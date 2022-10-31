546facc58b364b2596663445dfc7941d19f5c7e2
justinlugo <justinlugo.jl@gmail.com>
2022-10-30 21:03:12 -0400

    Teleporting w/ Thumbstick Works

---
+++ M-MO-VR Simulation/Assets/TeleportationManager.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TeleportationManager : MonoBehaviour
+{
+    [SerializeField] private InputActionAsset actionAsset;
+    [SerializeField] private XRRayInteractor rayInteractor;
+    [SerializeField] private TeleportationProvider provider;
+    private InputAction _thumbstick;
+    private bool _isActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rayInteractor.enabled = false;
+
+        var activate = actionAsset.FindActionMap("XRI LeftHand Locomotion").FindAction("Teleport Mode Activate");
+        activate.Enable();
+        activate.performed += OnTeleportActivate;
+
+        var cancel = actionAsset.FindActionMap("XRI LeftHand Locomotion").FindAction("Teleport Mode Cancel");
+        cancel.Enable();
+        cancel.performed += OnTeleportCancel;
+
+        _thumbstick = actionAsset.FindActionMap("XRI LeftHand Locomotion").FindAction("Move");
+        _thumbstick.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_isActive)
+        {
+            return;
+        }
+
+        if (_thumbstick.ReadValue<Vector2>() != Vector2.zero)
+        {
+            return;
+        }
+
+        //if (_thumbstick.triggered)
+        //{
+        //    return;
+        //}
+
+        if (!rayInteractor.TryGetCurrent3DRaycastHit(out RaycastHit hit))
+        {
+            rayInteractor.enabled = false;
+            _isActive = false;
+            return;
+        }
+
+        TeleportRequest request = new TeleportRequest()
+        {
+            destinationPosition = hit.point,
+        };
+
+        provider.QueueTeleportRequest(request);
+
+        rayInteractor.enabled = false;
+        _isActive = false;
+    }
+
+    private void OnTeleportActivate(InputAction.CallbackContext context)
+    {
+        rayInteractor.enabled = true;
+        _isActive = true;
+    }
+
+    private void OnTeleportCancel(InputAction.CallbackContext context)
+    {
+        rayInteractor.enabled = false;
+        _isActive = false;
+    }
+}
