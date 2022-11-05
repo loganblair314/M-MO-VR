983d92d9cf3a1c9290d6db4176ee5c2ead73d1e4
justinlugo <justinlugo.jl@gmail.com>
2022-11-04 21:54:29 -0400

    Basic Test
    Sorta works, will investigate further later.

---
+++ M-MO-VR Simulation/Assets/HapticTest.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class HapticTest : MonoBehaviour
+{
+    [Range(0,1)]
+    public float intensity;
+    public float duration;
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRBaseInteractable interactable = GetComponent<XRBaseInteractable>();
+        interactable.activated.AddListener(TriggerHaptic);
+    }
+
+    public void TriggerHaptic(BaseInteractionEventArgs eventArgs)
+    {
+        if (eventArgs.interactorObject is XRBaseControllerInteractor controllerInteractor)
+        {
+            TriggerHaptic(controllerInteractor.xrController);
+        }
+    }
+
+    public void TriggerHaptic(XRBaseController controller)
+    {
+        if (intensity > 0)
+        {
+            controller.SendHapticImpulse(intensity, duration);
+        }
+    }
+}
