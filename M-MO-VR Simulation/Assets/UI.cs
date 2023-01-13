77d624a5c0b2c26fbcf87c499ecdfd8b92aa5798
The0neTrueAsian <98611947+The0neTrueAsian@users.noreply.github.com>
2023-01-13 15:37:20 -0500

    UI Room Display Prototype Functional

--- M-MO-VR Simulation/Assets/UI.cs
+++ M-MO-VR Simulation/Assets/UI.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UI : MonoBehaviour
-{
+{   
+    public TextMeshProUGUI uiDisplay;
+    public Slider mainSlider;
+    public string text;
+    public int size = 32;
+    private bool hidden;
+
+    /*
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +24,23 @@ public class UI : MonoBehaviour
     {
         
     }
+    */
+    
+    public void toggle(){
+
+        if(hidden){
+            uiDisplay.text = text;
+            hidden = false;
+        }
+        else{
+            uiDisplay.text = "";
+            hidden = true;
+        }
+    }
+
+    public void changeFontSize(){
+        size = (int)mainSlider.value;
+        uiDisplay.fontSize = size;
+    }
+    
 }
