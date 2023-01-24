be2ce408aa30a24ebba80846a709353f05596501
The0neTrueAsian <98611947+The0neTrueAsian@users.noreply.github.com>
2023-01-24 12:54:48 -0500

    Initial Teleport Implementation

---
+++ M-MO-VR Simulation/Assets/TeleportManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportManager : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Current_Spawn;
+    
+    
+    
+    
+    public GameObject DDR_Spawn;
+
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //For initial testing, we will set the spawn to the display room
+        Current_Spawn = DDR_Spawn;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void resetPosition(){
+        Player.transform.position = Current_Spawn.transform.position;
+    }
+}
