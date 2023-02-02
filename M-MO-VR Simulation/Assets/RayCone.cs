ce163f148dcef01e931cda12c50e038324579825
DAntaeAronne <94494773+DAntaeAronne@users.noreply.github.com>
2023-02-02 04:48:27 -0500

    Sweep of Rays
    Should make a sweep of rays across a 60 degree angle, when testing with the mouse button as the input it can recognize that objects are hit, will need to test with headset to actually see if the raycasts show and if it works with left trigger as input as well

---
+++ M-MO-VR Simulation/Assets/RayCone.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RayCone : MonoBehaviour
+{
+    public float distance;
+    public int theAngle;
+    public int segments;
+    public Transform cam;
+    public InputActionProperty coneBut;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (coneBut.action.WasPressedThisFrame())
+        {
+            RaycastSweep();
+        }
+    }
+
+    void RaycastSweep()
+    {
+        Vector3 startPos = cam.position; // umm, start position !
+        Vector3 targetPos = Vector3.zero; // variable for calculated end position
+
+        int startAngle = -theAngle / 2; // half the angle to the Left of the forward
+        int finishAngle = theAngle / 2; // half the angle to the Right of the forward
+
+        // the gap between each ray (increment)
+        int inc = theAngle / segments;
+
+
+        // step through and find each target point
+        for (int i = startAngle; i < finishAngle; i += inc) // Angle from forward
+        {
+            targetPos = (Quaternion.Euler(0, i, 0) * cam.forward).normalized * distance;
+
+            // linecast between points
+            if (Physics.Raycast(startPos, targetPos))
+            {
+                Debug.Log("Hit ");
+            }
+
+            // to show ray just for testing
+            Debug.DrawRay(startPos, targetPos, Color.red, 8);
+        }
+    }
+}
