76708f72011355fce3407256c11de01632d06f76
justinlugo <justinlugo.jl@gmail.com>
2023-02-04 10:19:00 -0500

    TTS Bot Added

---
+++ M-MO-VR Simulation/Assets/TTSSpeakerInput.cs
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// Code adapted from Unity's original TTS Speaker Input.
+
+using UnityEngine;
+using UnityEngine.UI;
+using Facebook.WitAi.TTS.Utilities;
+using TMPro;
+
+
+public class TTSSpeakerInput : MonoBehaviour
+{
+    //[SerializeField] private Text _title;
+    //[SerializeField] private InputField _input;
+    [SerializeField] private TTSSpeaker _speaker;
+    [SerializeField] private TextMeshProUGUI textField;
+
+
+    // Either say the current phrase or stop talking/loading
+    public void SayPhrase()
+    {
+        _speaker.Speak(textField.text);
+
+        /*
+        if (_speaker.IsLoading || _speaker.IsSpeaking)
+        {
+            _speaker.Stop();
+        }
+        else
+        {
+            _speaker.Speak(textField.text);
+        }
+        */
+    }
+}
