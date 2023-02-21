de0a6391b6fbbe0f782ddb872ca01d72adda1018
The0neTrueAsian <98611947+The0neTrueAsian@users.noreply.github.com>
2023-02-21 18:31:12 -0500

    Description Boards Complete

--- M-MO-VR Simulation/Assets/DisplayBoard.cs
+++ M-MO-VR Simulation/Assets/DisplayBoard.cs
@@ -9,14 +9,25 @@ public class DisplayBoard : MonoBehaviour
     public TextMeshProUGUI board;
     public string text;
 
+    public GameObject UI_Manager; 
+    UI ui;
+
     public Slider mainSlider;
 
     public int size = 5;
+    [SerializeField] private int index;
 
     
     // Start is called before the first frame update
     void Start()
     {
+        if(UI_Manager != null && UI_Manager.GetComponent<UI>() != null){
+            ui = UI_Manager.GetComponent<UI>();
+
+            text = ui.text[index];
+        }
+        
+        
         board.text = text;
     }
 
