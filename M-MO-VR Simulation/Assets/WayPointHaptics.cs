53f79a6d4da6d2d24692d08f9b7d60e20293df53
justinlugo <justinlugo.jl@gmail.com>
2023-03-01 19:30:06 -0500

    Waypoint Haptics Implemented
    Prototyped Waypoint Haptics; works as intended and ready for feedback.

---
+++ M-MO-VR Simulation/Assets/WayPointHaptics.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using TMPro;
+using System.Linq;
+
+public class WayPointHaptics : MonoBehaviour
+{
+    [SerializeField] XRController controller;
+    private int count;
+    private Animator animatorController;
+    private InputDevice targetDevice;
+    public Transform raycastOrigin;
+    public string objectTag = "Player";
+    public GameObject waypoint;
+    private bool activeWaypoint;
+    private float timer;
+
+    private void Start()
+    {
+        waypoint.SetActive(true);
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == objectTag)
+        {
+           waypoint.SetActive(false);
+            Debug.Log("Disabling waypoint.");
+        }
+    }
+
+    private void Update()
+    {
+        // Quest 2 takes a few seconds to show up in Unity, so have to intialize all of this in Update.
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) && triggerValue > 0.1f)
+        {
+            RaycastHit hit;
+            Ray ray = new Ray(raycastOrigin.position, raycastOrigin.forward);
+
+            // If a Raycast of length 2 detectes a hit.
+            if (Physics.Raycast(ray, out hit, 10))
+            {
+                var hitPoint = hit.point;
+                hitPoint.y = 0;
+
+                var playerPosition = raycastOrigin.position;
+                playerPosition.y = 0;
+
+                float distance = Vector3.Distance(hitPoint, playerPosition);
+
+                if ((hit.transform.tag == "Waypoint") && (waypoint.activeSelf))
+                {
+                    timer += Time.deltaTime;
+                    if (timer > 0.5)
+                    {
+                        Debug.Log("Detected waypoint.");
+                        targetDevice.SendHapticImpulse(0, 1.0f, 0.1f);
+                        timer = 0;
+                    }
+                }
+                else
+                {
+                    if (((TeleportManager.index == 2) || (TeleportManager.index == 3) || (TeleportManager.index == 4) || (TeleportManager.index == 5)) && (!MenuManager.MenuOpen))
+                    {
+                        if (distance > 0 && distance <= 0.67)
+                        {
+                            Debug.Log("Item is between 0 and 0.67 units away.");
+                            targetDevice.SendHapticImpulse(0, 1.0f, 0.1f);
+                        }
+                        else if (distance > 0.67 && distance <= 1.33)
+                        {
+                            Debug.Log("Item is between 0.67 and 1.33 units away.");
+                            targetDevice.SendHapticImpulse(0, 0.67f, 0.1f);
+                        }
+                        else if (distance > 1.33 && distance < 2.0)
+                        {
+                            Debug.Log("Item is between 1.33 and 2.0 units away.");
+                            targetDevice.SendHapticImpulse(0, 0.33f, 0.1f);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
